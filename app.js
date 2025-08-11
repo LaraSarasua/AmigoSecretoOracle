4f90395b1b0f13ecbbe62d36e10fb9ad8ebee599
LaraSarasua <lara.sarasua05@gmail.com>
2025-08-11 20:11:42 -0300

    Se agregó la funcion de sortear amigos pero dejó de funcionar todas las funciones

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/* global agregarAmigo, sortearAmigo */
 //array para guardar los nombres 
 let Amigos = []
 //funcion para agregar los nombres a la lista
@@ -18,11 +19,25 @@ function agregarAmigo() {
 }   
 
 function mostrarLista(){
-    let lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";
+    let lista = document.getElementById("listaAmigos"); //donde aparecen los nombres
+    lista.innerHTML = ""; //limpia la lista
     Amigos.forEach(amigo => {
-        let li = document.createElement("li");
-        li.textContent = amigo;
-        lista.appendChild(li);
+        let li = document.createElement("li"); //crea el LI
+        li.textContent = amigo; //asigna el nombre
+        lista.appendChild(li); //agrega a la lista
     });
+}
+
+//creamos funcion para sortear los amigos
+function sortearAmigo() {
+    if(Amigos.length === 0) {
+        alert("Se necesita que agregue amigos para poder sortear");
+        return;
+    }
+    let Random = Math.floor(Math.random() * Amigos.length);
+
+    let amigoSorteado = Amigos[Random];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = <li>'El amigo elegido es: ${amigoSorteado}</li> //No me dejaba usar + asi que utilice el $ 
 }
